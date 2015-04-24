15239b0fab33708a5f295fd727a4edfbe42fb827
Yannick Lemmens <lemmens.9@gmail.com>
2015-04-24 11:06:15 +0200

    navbar bootstrap added

--- CodeIgniter/application/views/home_view.php
+++ CodeIgniter/application/views/home_view.php
@@ -3,10 +3,33 @@
 <head>
 <title>TEDxPXL</title>
 <meta charset="UTF-8"/>
-<img src="http://www.tedxuhasselt.eu/themes/tedx/logo.png"/>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+<nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">TEDxPXL</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
 </head>
 <body>
-<h1>test</h1>
+
 
 
 </body>
