309ae38d4151203dc696e8e3289c4b430ef79d54
kevindeleo <kevin.deleo@student.pxl.be>
2015-05-16 18:47:49 +0200

    kutforum

---
+++ CodeIgniter/application/logs/log-2015-05-16.php
@@ -0,0 +1,55 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2015-05-16 17:53:28 --> 404 Page Not Found: Assets/images
+ERROR - 2015-05-16 17:56:03 --> 404 Page Not Found: Assets/images
+ERROR - 2015-05-16 17:57:59 --> 404 Page Not Found: Stylecss/index
+ERROR - 2015-05-16 18:01:35 --> 404 Page Not Found: Stylecss/index
+ERROR - 2015-05-16 18:02:03 --> 404 Page Not Found: Stylecss/index
+ERROR - 2015-05-16 18:02:05 --> 404 Page Not Found: Stylecss/index
+ERROR - 2015-05-16 18:02:07 --> 404 Page Not Found: Indexphp/index
+ERROR - 2015-05-16 18:02:11 --> 404 Page Not Found: Forum_topicphp/index
+ERROR - 2015-05-16 18:02:27 --> 404 Page Not Found: Stylecss/index
+ERROR - 2015-05-16 18:02:27 --> 404 Page Not Found: Forum/index.php
+ERROR - 2015-05-16 18:06:20 --> 404 Page Not Found: Stylecss/index
+ERROR - 2015-05-16 18:11:00 --> 404 Page Not Found: Stylecss/index
+ERROR - 2015-05-16 18:11:02 --> 404 Page Not Found: Topic/index
+ERROR - 2015-05-16 18:11:55 --> 404 Page Not Found: Stylecss/index
+ERROR - 2015-05-16 18:11:55 --> 404 Page Not Found: Stylecss/index
+ERROR - 2015-05-16 18:11:56 --> 404 Page Not Found: Stylecss/index
+ERROR - 2015-05-16 18:12:01 --> 404 Page Not Found: Topic/index
+ERROR - 2015-05-16 18:12:13 --> 404 Page Not Found: Stylecss/index
+ERROR - 2015-05-16 18:13:19 --> 404 Page Not Found: Stylecss/index
+ERROR - 2015-05-16 18:13:20 --> 404 Page Not Found: Forum_topic/index
+ERROR - 2015-05-16 18:13:43 --> 404 Page Not Found: Stylecss/index
+ERROR - 2015-05-16 18:13:45 --> 404 Page Not Found: Stylecss/index
+ERROR - 2015-05-16 18:13:47 --> 404 Page Not Found: Stylecss/index
+ERROR - 2015-05-16 18:14:29 --> 404 Page Not Found: Stylecss/index
+ERROR - 2015-05-16 18:14:34 --> 404 Page Not Found: Create_catphp/index
+ERROR - 2015-05-16 18:14:37 --> 404 Page Not Found: Stylecss/index
+ERROR - 2015-05-16 18:14:38 --> 404 Page Not Found: Stylecss/index
+ERROR - 2015-05-16 18:14:40 --> 404 Page Not Found: Topic/index
+ERROR - 2015-05-16 18:15:37 --> 404 Page Not Found: Stylecss/index
+ERROR - 2015-05-16 18:15:38 --> Severity: Notice --> Undefined index: signed_in C:\xampp\htdocs\WebAdvTED\CodeIgniter\application\views\forum_topic.php 5
+ERROR - 2015-05-16 18:15:38 --> 404 Page Not Found: Stylecss/index
+ERROR - 2015-05-16 18:15:48 --> Severity: Notice --> Undefined index: signed_in C:\xampp\htdocs\WebAdvTED\CodeIgniter\application\views\forum_topic.php 5
+ERROR - 2015-05-16 18:15:53 --> Severity: Notice --> Undefined index: signed_in C:\xampp\htdocs\WebAdvTED\CodeIgniter\application\views\forum_topic.php 5
+ERROR - 2015-05-16 18:15:53 --> 404 Page Not Found: Stylecss/index
+ERROR - 2015-05-16 18:18:03 --> 404 Page Not Found: Stylecss/index
+ERROR - 2015-05-16 18:18:04 --> Severity: Notice --> Undefined index: signed_in C:\xampp\htdocs\WebAdvTED\CodeIgniter\application\views\forum_topic.php 5
+ERROR - 2015-05-16 18:18:04 --> 404 Page Not Found: Stylecss/index
+ERROR - 2015-05-16 18:18:07 --> 404 Page Not Found: Create_catphp/index
+ERROR - 2015-05-16 18:20:20 --> 404 Page Not Found: Stylecss/index
+ERROR - 2015-05-16 18:20:20 --> 404 Page Not Found: Create_catphp/index
+ERROR - 2015-05-16 18:21:28 --> Severity: 8192 --> mysql_query(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\xampp\htdocs\WebAdvTED\CodeIgniter\application\views\forum_cat.php 10
+ERROR - 2015-05-16 18:21:30 --> Severity: Warning --> mysql_query(): Kan geen verbinding maken omdat de doelcomputer de verbinding actief heeft geweigerd.
+ C:\xampp\htdocs\WebAdvTED\CodeIgniter\application\views\forum_cat.php 10
+ERROR - 2015-05-16 18:21:30 --> Severity: Warning --> mysql_query(): A link to the server could not be established C:\xampp\htdocs\WebAdvTED\CodeIgniter\application\views\forum_cat.php 10
+ERROR - 2015-05-16 18:21:30 --> 404 Page Not Found: Stylecss/index
+ERROR - 2015-05-16 18:22:34 --> Severity: Notice --> Undefined index: signed_in C:\xampp\htdocs\WebAdvTED\CodeIgniter\application\views\forum_topic.php 5
+ERROR - 2015-05-16 18:22:34 --> 404 Page Not Found: Stylecss/index
+ERROR - 2015-05-16 18:23:00 --> Severity: Parsing Error --> syntax error, unexpected 'else' (T_ELSE) C:\xampp\htdocs\WebAdvTED\CodeIgniter\application\views\forum_topic.php 6
+ERROR - 2015-05-16 18:23:10 --> Severity: Notice --> Undefined index: signed_in C:\xampp\htdocs\WebAdvTED\CodeIgniter\application\views\forum_topic.php 5
+ERROR - 2015-05-16 18:23:10 --> 404 Page Not Found: Stylecss/index
+ERROR - 2015-05-16 18:37:50 --> 404 Page Not Found: Topic/index
+ERROR - 2015-05-16 18:37:51 --> 404 Page Not Found: Forum/index
+ERROR - 2015-05-16 18:37:54 --> 404 Page Not Found: Forum/index
