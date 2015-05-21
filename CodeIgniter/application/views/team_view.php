209dc041cc028f49a0de9815f87f209154bbd3f9
DieterPossen <dieterboy_9@hotmail.com>
2015-05-21 17:45:39 +0200

    kleine aanpassingsn

--- CodeIgniter/application/views/team_view.php
+++ CodeIgniter/application/views/team_view.php
@@ -1,11 +1,9 @@
 <style>
 #team {
-		border-style : inset;
-		border-width : 5px;
-		border-color : #00000;
-		-moz-border-radius12px;
-		-webkit-border radius:12px;
-		border-radius:12px;
+
+		-moz-border-radius:8px;
+		-webkit-border radius:8px;
+		border-radius:8px;
 		width : 300px;
 		display : inline-block;
 		margin : 10px;
@@ -29,7 +27,7 @@ h2, p {
 
  <?php foreach($admins as $rows) 
  {?>
- <div id="team" class="panel-default">
+ <div id="team" class="showback">
 	<a href="<?php echo site_url('profile/user')."/".$rows->id?>">
 			 <?php $image = $rows ->image ;
 
