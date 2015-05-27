9b2ad6420d1ee3079215923f1115c7172c7ce829
BrentMellaerts <brent.mellaerts@student.pxl.be>
2015-05-27 22:27:01 +0200

    ...

--- CodeIgniter/application/logs/log-2015-05-21.php
+++ CodeIgniter/application/logs/log-2015-05-21.php
@@ -1,3 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
 ERROR - 2015-05-21 11:46:04 --> 404 Page Not Found: Forumcategorie/index
+ERROR - 2015-05-21 13:16:01 --> 404 Page Not Found: Assets/images
+ERROR - 2015-05-21 13:16:01 --> 404 Page Not Found: Assets/images
+ERROR - 2015-05-21 13:17:22 --> 404 Page Not Found: Admin/user
